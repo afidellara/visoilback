ce0cb677f0ea7b1e24689bbf4267bf930455c1d2
Jesus De La Cruz <jesusdaviddelacruzarroyo@yahoo.com>
2023-10-04 21:47:42 -0500

    fix Validaciones de Inicio Sesion

--- controllers/Auth/ClienteAuthController.js
+++ controllers/Auth/ClienteAuthController.js
@@ -6,21 +6,23 @@ exports.iniciarSesion = async function (req, res) {
   const { correo, pass } = req.body;
 
   try {
-    // Buscar el empleado por el correo proporcionado
+    // Buscar el cliente por el correo proporcionado
     const cliente = await Cliente.findOne({ correo });
-
-    // Verificar si el empleado existe y si la pass es válida
-    if (cliente) {
-      const passwordMatch = await bcrypt.compare(pass, cliente.pass);
-      if (passwordMatch) {
-        // Aquí podrías realizar acciones adicionales, como generar un token de autenticación, etc.
-        // Devolver una respuesta con un código de éxito y los datos del empleado
-        return res.status(200).json({ success: true, cliente });
-      }
+    
+    // Verificar si el cliente existe
+    if (!cliente) {
+      return res.status(404).json({ success: false, message: 'Correo no encontrado' });
     }
 
-    // Si el empleado no existe o las credenciales son incorrectas, devolver una respuesta con un código de error
-    return res.status(401).json({ success: false, message: 'Credenciales inválidas' });
+    // Si el cliente existe, verificar la contraseña
+    const passwordMatch = await bcrypt.compare(pass, cliente.pass);
+    if (passwordMatch) {
+      // Aquí podrías realizar acciones adicionales, como generar un token de autenticación, etc.
+      // Devolver una respuesta con un código de éxito y los datos del cliente
+      return res.status(200).json({ success: true, cliente });
+    } else {
+      return res.status(401).json({ success: false, message: 'Contraseña Incorrecta' });
+    }
   } catch (error) {
     // Manejar errores en caso de que haya un problema con la base de datos u otros errores inesperados
     return res.status(500).json({ success: false, message: 'Error en el servidor' });
